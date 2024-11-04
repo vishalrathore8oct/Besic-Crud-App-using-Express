f205bd68a4a293470fb13bee29e46eaacbfcbf69
vishalrathore8oct <vishal.rathore8oct@gmail.com>
2024-11-04 19:47:00 +0530

    add the custom logger for log the output

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 import 'dotenv/config'
 import express from "express";
+import morgan from "morgan";
+import logger from "./logger.js";
 
 
 const app = express()
 const PORT = process.env.PORT || 3000
 
 app.use(express.json())
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 // add the id automaticly
 let nextId = 3 
